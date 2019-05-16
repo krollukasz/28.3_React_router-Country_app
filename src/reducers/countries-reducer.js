6a7d996650ed5912bbeccf659e963b32d92773f8
Łukasz Król <lionel@o2.pl>
2019-05-16 23:28:32 +0200

    Add set continents

--- src/reducers/countries-reducer.js
+++ src/reducers/countries-reducer.js
@@ -1,10 +1,10 @@
-import { GET_COUNTRIES, GET_COUNTRY, SEARCH_COUNTRIES, DELETE_COUNTRY } from "../actions/actions-countries";
+import { GET_COUNTRIES, GET_COUNTRY, SEARCH_COUNTRIES, DELETE_COUNTRY, SET_CONTINENT } from "../actions/actions-countries";
 import countriesData from "../data/countries.json";
 
 const initialState = {
   countries: countriesData,
   selectedCountry: {},
-  visibleCountries: countriesData
+  visibleCountries: []
 };
 
 const countriesReducer = function (state = initialState, action) {
@@ -13,7 +13,7 @@ const countriesReducer = function (state = initialState, action) {
       return Object.assign({}, state, {countries: state.countries})
     
     case GET_COUNTRY:
-      const selectedCountry = state.countries.find(country => country.id === parseInt(action.id));
+      const selectedCountry = state.countries.find(country => country.id == action.id);
       return Object.assign({}, state, { selectedCountry });
 
     case SEARCH_COUNTRIES:
@@ -24,6 +24,10 @@ const countriesReducer = function (state = initialState, action) {
       const notDeletedCountries = state.countries.filter(country => country.id != action.id);
       const notDeletedVisibleCountries = state.visibleCountries.filter(country => country.id != action.id);
       return Object.assign({}, state, {countries: notDeletedCountries, visibleCountries: notDeletedVisibleCountries});
+
+    case SET_CONTINENT:
+      const continentCountries = state.countries.filter(country => country.continent === action.name);
+      return Object.assign({}, state, {visibleCountries: continentCountries});
   }
 
   return state;
